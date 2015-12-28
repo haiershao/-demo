30851de48a077aadcb31956406481d2abfff4b6a
liuhai <1434591364@qq.com>
2015-12-28 07:18:42 -0800

    初始化项目

---
+++ mybibibi/mybibibi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mybibibi
+//
+//  Created by 刘海 on 15/12/28.
+//  Copyright © 2015年 刘海. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mybibibi/mybibibi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mybibibi
+//
+//  Created by 刘海 on 15/12/28.
+//  Copyright © 2015年 刘海. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
